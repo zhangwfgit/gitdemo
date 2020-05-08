b848f21c01a683bf17e7028c0f09d15ec58ad19a
zhangWeiFeng <zhangweifeng@ruanwe.onaliyun.com>
2020-05-08 15:31:31 +0800

    初始导入

---
+++ src/com/zhangwf/test/GitTestDemo.java
@@ -0,0 +1,13 @@
+package com.zhangwf.test;
+
+public class GitTestDemo {
+
+    public static void main(String[] args) {
+
+        method1();
+    }
+
+    public static void method1(){
+        System.out.println("this is just a test!");
+    }
+}
