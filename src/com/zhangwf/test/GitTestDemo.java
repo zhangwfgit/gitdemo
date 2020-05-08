f541a191cffd3c426761f8b3b34ca1dffc036f34
zhangWeiFeng <zhangweifeng@ruanwe.onaliyun.com>
2020-05-08 16:29:36 +0800

    feature/lisi提交补充

--- src/com/zhangwf/test/GitTestDemo.java
+++ src/com/zhangwf/test/GitTestDemo.java
@@ -10,4 +10,8 @@ public class GitTestDemo {
     public static void method1(){
         System.out.println("this is just a test!");
     }
+    public static void method3(){
+        System.out.println("this is just a test1");
+        System.out.println("this is just a test3");
+    }
 }
